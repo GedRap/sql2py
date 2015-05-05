878bc119105843bfe385f5810851df46d33ebd21
Gediminas Rapolavicius <gediminas.rap@gmail.com>
2015-05-05 21:21:09 +0300

    Hello world

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='sql2py',
+    version='0.1',
+    long_description="Covert SQL queries to Python objects",
+    packages=['sql2py'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[]
+)

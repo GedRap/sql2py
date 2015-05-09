56e8e6518919707c5ed37bc6b3cbb0c91bbdfa5b
Gediminas Rapolavicius <gediminas.rap@gmail.com>
2015-05-10 00:02:18 +0300

    order by implementation

--- sql2py/sql_parser.py
+++ sql2py/sql_parser.py
@@ -8,16 +8,19 @@ def parse_select_query(query):
     select_keyword_token = Keyword("select", caseless=True)
     from_keyword_token = Keyword("from", caseless=True)
     limit_keyword_token = Keyword("limit", caseless=True)
+    order_by_keyword_token = Keyword("order by", caseless=True)
 
     identifier_token = Word(alphanums + "_").setName("identifier")
     column_name_tokens = Group(delimitedList(identifier_token, ","))
     table_name_token = Word(alphanums + "_").setName("table_name")
+    order_by_token = order_by_keyword_token + column_name_tokens.setResultsName("order_by_cols") + Optional((Keyword("asc", caseless=True).setResultsName("order_by_type") | Keyword("desc", caseless=True).setResultsName("order_by_type")))
 
     limit_token = limit_keyword_token + Optional(Word(nums).setResultsName("offset") + Literal(",")) \
                   + Word(nums).setResultsName("rows_limit")
 
     select_grammar << (select_keyword_token + ('*' | column_name_tokens).setResultsName("columns")\
                      + from_keyword_token + table_name_token.setResultsName("table"))\
+                     + Optional(order_by_token).setResultsName("order")\
                      + Optional(limit_token).setResultsName("limit")
 
     parsed_query = select_grammar.parseString(query)
@@ -30,5 +33,12 @@ def parse_select_query(query):
         if parsed_query.limit.offset is not "":
             select_query.offset = int(parsed_query.limit.offset)
         select_query.rows_limit = int(parsed_query.limit.rows_limit)
+        
+    if parsed_query.order is not "":
+        select_query.order_columns = parsed_query.order.order_by_cols
+        if parsed_query.order.order_by_type is not "":
+            select_query.order_type = parsed_query.order.order_by_type
+        else:
+            select_query.order_type = "asc"
 
     return select_query
--- tests/parsing/select_tests.py
+++ tests/parsing/select_tests.py
@@ -11,6 +11,9 @@ class SimpleSelectsTestCase(unittest.TestCase):
         self.assertEqual(parsed.columns[1], "col2")
         self.assertEqual(len(parsed.columns), 2)
         self.assertEqual(parsed.table_name, "tab1")
+        self.assertIsNone(parsed.offset)
+        self.assertIsNone(parsed.order_type)
+        self.assertEqual(len(parsed.order_columns), 0)
 
     def test_underscore_in_identifiers(self):
         parsed = parse_select_query("select col_1 from tab_1")
@@ -29,6 +32,7 @@ class SimpleSelectsTestCase(unittest.TestCase):
         parsed = parse_select_query("select col1 from tab1 limit 15")
 
         self.assertEqual(parsed.rows_limit, 15)
+        self.assertIsNone(parsed.offset)
 
     def test_limit_condition_both_offset_and_limit_given(self):
         parsed = parse_select_query("select col1 from tab1 limit 1, 5")
@@ -36,5 +40,33 @@ class SimpleSelectsTestCase(unittest.TestCase):
         self.assertEqual(parsed.rows_limit, 5)
         self.assertEqual(parsed.offset, 1)
 
+    def test_order_clause_implicit(self):
+        parsed = parse_select_query("select col1 from tab1 order by col1")
+
+        self.assertEqual(len(parsed.order_columns), 1)
+        self.assertEqual(parsed.order_columns[0], "col1")
+        self.assertEqual(parsed.order_type, "asc")
+
+    def test_order_clause_asc(self):
+        parsed = parse_select_query("select col1 from tab1 order by col1 asc")
+
+        self.assertEqual(len(parsed.order_columns), 1)
+        self.assertEqual(parsed.order_columns[0], "col1")
+        self.assertEqual(parsed.order_type, "asc")
+
+    def test_order_clause_desc(self):
+        parsed = parse_select_query("select col1 from tab1 order by col1 desc")
+
+        self.assertEqual(len(parsed.order_columns), 1)
+        self.assertEqual(parsed.order_columns[0], "col1")
+        self.assertEqual(parsed.order_type, "desc")
+
+    def test_order_clause_multi_column(self):
+        parsed = parse_select_query("select col1, col2 from tab1 order by col2, col1 asc")
+
+        self.assertEqual(len(parsed.order_columns), 2)
+        self.assertEqual(parsed.order_columns[0], "col2")
+        self.assertEqual(parsed.order_columns[1], "col1")
+        self.assertEqual(parsed.order_type, "asc")
 if __name__ == '__main__':
     unittest.main()
